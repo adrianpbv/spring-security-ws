4e9687e53ed1e7f7d4707554590ea9984a9cd0ef
adrianj <adrianpbv@gmail.com>
2024-09-29 10:30:09 -0400

    configure inMemory authentication

---
+++ spring-sec3/src/test/java/com/adrianj/springsec1/SpringSec3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianj.springsec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSec3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
