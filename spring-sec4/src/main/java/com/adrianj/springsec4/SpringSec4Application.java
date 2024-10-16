6462c055df88d5d1a635bc51586c943c0660f44c
adrianj <adrianpbv@gmail.com>
2024-10-16 08:25:53 -0400

    ss4 authenticate users with custom UserDetailsService. Register new user, encrypt pwd with PasswordEncoder and save to DB with JPA

---
+++ spring-sec4/src/main/java/com/adrianj/springsec4/SpringSec4Application.java
@@ -0,0 +1,13 @@
+package com.adrianj.springsec4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSec4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSec4Application.class, args);
+    }
+
+}
