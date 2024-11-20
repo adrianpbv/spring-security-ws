e9722f94787910619b4d4dbd611bebb90e2967ab
adrianj <adrianpbv@gmail.com>
2024-11-19 22:55:19 -0500

    use SecurityContext to get authentication details

--- section7/springsecsection7/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section7/springsecsection7/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,10 +1,19 @@
 package com.eazybytes;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 public class EazyBankBackendApplication {
+
+    @Bean
+    public InitializingBean initializingSecurityContextMode() {
+        // MODE_THREADLOCAL is the default value. Each thread (per-request) has its own auth details in the SecurityContext
+        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EazyBankBackendApplication.class, args);
--- section7/springsecsection7/src/main/java/com/eazybytes/controller/AccountController.java
+++ section7/springsecsection7/src/main/java/com/eazybytes/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.eazybytes.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,16 @@ public class AccountController {
         return "Here are the account details from the DB";
     }
 
+    @GetMapping("/myUserName")
+    public String getUserName() {
+        // Get authentication from the securityContext. It contains user's details like username and role
+        org.springframework.security.core.Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
+    @GetMapping("/myUserName1")
+    public String getUserName1(Authentication authentication) {
+
+        return authentication.getName();
+    }
 }
